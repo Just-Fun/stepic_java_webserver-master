0e415200380c09488c60c76630ae57d452a7d94a
Just-Fun <Just-Fun@users.noreply.github.com>
2016-08-19 23:35:39 +0300

    start

---
+++ L1.1 Simple web server/src/main/java/main/Main.java
@@ -0,0 +1,30 @@
+package main;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.AllRequestsServlet;
+import servlets.AllRequestsServlet2;
+
+/**
+ * @author v.chibrikov
+ *         <p>
+ *         Пример кода для курса на https://stepic.org/
+ *         <p>
+ *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        AllRequestsServlet2 allRequestsServlet = new AllRequestsServlet2();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        java.util.logging.Logger.getGlobal().info("Server started");
+        server.join();
+        // http://localhost:8080/mirror?key=test
+    }
+}
